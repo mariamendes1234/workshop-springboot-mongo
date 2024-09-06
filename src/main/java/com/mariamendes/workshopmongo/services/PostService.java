8e244680575b70de87c3c21051b8668e1cee7121
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-06 11:55:36 +0100

    Consulta simples com query methods

--- src/main/java/com/mariamendes/workshopmongo/services/PostService.java
+++ src/main/java/com/mariamendes/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mariamendes.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public List<Post> findByTitle(String text){
+		//http://localhost:8080/posts/search?test=bom%20dia (codificado)
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
