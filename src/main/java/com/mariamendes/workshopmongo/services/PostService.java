69b5a97769fc0d223238229957e0f17c2c48e1b7
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-06 12:18:31 +0100

    Consulta com vários critérios

--- src/main/java/com/mariamendes/workshopmongo/services/PostService.java
+++ src/main/java/com/mariamendes/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mariamendes.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,9 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		//http://localhost:8080/posts/search?test=bom%20dia (codificado)
 		return repo.findByTitle(text);
+	}
+	public List<Post> fullSearch (String text, Date minDate, Date maxDate ){
+		maxDate = new Date(maxDate.getTime() + 24 *60*60*1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
